00f48e8714cb06da7e43cf5384e818c1195d98f7
Matheus Germano da Costa <62772781+matheus-germano@users.noreply.github.com>
2021-06-25 16:00:28 -0300

    Project File Structure and Question Component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
 import { useHistory } from 'react-router-dom'
 import { FormEvent, useState } from 'react'
-import { useAuth } from '../hooks/useAuth'
-import { database } from '../services/firebase'
+import { useAuth } from '../../hooks/useAuth'
+import { database } from '../../services/firebase'
 
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
+import illustrationImg from '../../assets/images/illustration.svg'
+import logoImg from '../../assets/images/logo.svg'
+import googleIconImg from '../../assets/images/google-icon.svg'
 
-import { Button } from '../components/Button'
+import { Button } from '../../components/Button'
 
-import '../styles/auth.scss'
+import './styles.scss'
 
 
 export function Home() {
--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,14 +1,14 @@
 import { Link, useHistory } from 'react-router-dom'
 import { FormEvent, useState } from 'react'
-import { useAuth } from '../hooks/useAuth'
-import { database } from '../services/firebase'
+import { useAuth } from '../../hooks/useAuth'
+import { database } from '../../services/firebase'
 
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
+import illustrationImg from '../../assets/images/illustration.svg'
+import logoImg from '../../assets/images/logo.svg'
 
-import { Button } from '../components/Button'
+import { Button } from '../../components/Button'
 
-import '../styles/auth.scss'
+import './styles.scss'
 
 export function NewRoom() {
   const { user } = useAuth()
